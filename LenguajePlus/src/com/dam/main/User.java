2bb5fd253e44be727ce2f9e888c034a81d5cddeb
CindyTaboada <cindy.taboada@hotmail.com>
2024-03-29 13:29:43 +0100

    modificación clase User

--- LenguajePlus/src/com/dam/main/User.java
+++ LenguajePlus/src/com/dam/main/User.java
@@ -7,4 +7,32 @@ public class User {
 	protected String password;
 	
 	//Constructor
-	p
+	public User(String email, String password) {
+		super();
+		this.email = email;
+		this.password = password;
+	}
+	
+	//Getter
+	public String getEmail() {
+		return email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	
+	//toString
+	@Override
+	public String toString() {
+		return "Email: " + email + "Password:" + password;
+	}
+	
+	
+		
+		
+		
+		
+
+	}
+
+
