954522c14612e931a3cc6cbdbda9a2d7f0cf6261
LOUBIT <103092618+LucasOU21@users.noreply.github.com>
2024-04-08 18:22:55 +0200

    Commit Interfaz Niveles y Dificultad

--- Niveles/src/dam/main/GestionApp.java
+++ Niveles/src/dam/main/GestionApp.java
@@ -1,10 +1,28 @@
 package dam.main;
 
+import java.awt.EventQueue;
+
+import dam.control.VListenerNiveles;
+import dam.view.VPrincipalDificultad;
+import dam.view.VPrincipalLenguaje;
+
 public class GestionApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
 
+				VPrincipalLenguaje vNiveles = new VPrincipalLenguaje();
+				VPrincipalDificultad vDificultad = new VPrincipalDificultad();
+
+				VListenerNiveles listener = new VListenerNiveles(vNiveles, vDificultad);
+
+				vNiveles.setListener(listener);
+
+				vNiveles.hacerVisible();
+
+			}
+
+		});
 	}
-
 }
