70b9fffca1467627f460e2ad14b92071668e142e
CindyTaboada <cindy.taboada@hotmail.com>
2024-05-28 22:00:43 +0200

    PreparacionClasePersistencia

--- Proyecto/src/db/AccesoBBDD.java
+++ Proyecto/src/db/AccesoBBDD.java
@@ -1,3 +1,4 @@
+package db;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
--- Proyecto/src/db/LenguajePlusPersistencia.java
+++ Proyecto/src/db/LenguajePlusPersistencia.java
@@ -1,3 +1,4 @@
+package db;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
