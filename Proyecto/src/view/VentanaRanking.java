a6f924dc95077ac58c23b9ea3ccac60ce645f8eb
LOUBIT <103092618+LucasOU21@users.noreply.github.com>
2024-05-31 12:56:01 +0200

    Creado las carpet pojos, y modifica el JFrame Ranking

--- Proyecto/src/view/VentanaRanking.java
+++ Proyecto/src/view/VentanaRanking.java
@@ -17,6 +17,8 @@ import java.awt.Toolkit;
 import java.awt.Color;
 import javax.swing.JTable;
 import javax.swing.SwingConstants;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
 
 public class VentanaRanking extends JFrame {
 
@@ -28,6 +30,8 @@ public class VentanaRanking extends JFrame {
 	private JButton btnSalirRanking;
 	private JScrollPane scrpContenedor;
 	private JTable tblRanking;
+	private JButton btnConsultar;
+	private JComboBox <String>comboBRanking;
 
 	public VentanaRanking() {
 		getContentPane().setBackground(new Color(51, 255, 102));
@@ -43,22 +47,33 @@ public class VentanaRanking extends JFrame {
 		setBackground(SystemColor.controlHighlight);
 		getContentPane().setLayout(null);
 
-		lblRanking = new JLabel("Ranking");
+		lblRanking = new JLabel("RANKING");
 		lblRanking.setHorizontalAlignment(SwingConstants.CENTER);
 		lblRanking.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		lblRanking.setBounds(254, 10, 78, 51);
+		lblRanking.setBounds(214, 24, 116, 51);
 		getContentPane().add(lblRanking);
 
 		btnSalirRanking = new JButton("Salir del Ranking");
-		btnSalirRanking.setBounds(228, 279, 129, 21);
+		btnSalirRanking.setFont(new Font("Tahoma", Font.BOLD, 13));
+		btnSalirRanking.setBounds(228, 279, 143, 21);
 		getContentPane().add(btnSalirRanking);
 		
 		JScrollPane scrpRanking = new JScrollPane();
-		scrpRanking.setBounds(65, 66, 455, 181);
+		scrpRanking.setBounds(65, 85, 455, 181);
 		getContentPane().add(scrpRanking);
 		
 		tblRanking = new JTable();
 		scrpRanking.setViewportView(tblRanking);
+		
+		btnConsultar = new JButton("Consultar");
+		btnConsultar.setFont(new Font("Tahoma", Font.BOLD, 13));
+		btnConsultar.setBounds(65, 16, 99, 28);
+		getContentPane().add(btnConsultar);
+		
+		comboBRanking = new JComboBox();
+		comboBRanking.setModel(new DefaultComboBoxModel<String>(new String[] {"TODAS", "TOP 10 N1", ""}));
+		comboBRanking.setBounds(75, 54, 59, 21);
+		getContentPane().add(comboBRanking);
 	}
 	
 	private void crearComponentes() {
