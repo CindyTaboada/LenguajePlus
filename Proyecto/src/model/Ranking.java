ee0fd3521d6d6b7f521e4f4e84fccab0146db220
LOUBIT <103092618+LucasOU21@users.noreply.github.com>
2024-06-01 22:28:56 +0200

    Ranking ventana actualizada

--- Proyecto/src/model/Ranking.java
+++ Proyecto/src/model/Ranking.java
@@ -1,5 +1,31 @@
 package model;
 
 public class Ranking {
+	
+	private String usuario;
+    private int lenjuages;
 
+    public Ranking(String usuario, int lenjuages) {
+        this.usuario = usuario;
+        this.lenjuages = lenjuages;
+        
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+	public int getLenjuages() {
+		return lenjuages;
+	}
+
+	public void setLenjuages(int lenjuages) {
+		this.lenjuages = lenjuages;
+	}
+
+   
 }
